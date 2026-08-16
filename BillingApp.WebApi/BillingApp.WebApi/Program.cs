14984d7249528b2d88c3cc58211e6e08bd78f884
Ksenia Danilets <marsi265@inbox.lv>
2026-08-16 21:43:42 +0300

    Feat(api): add orders controller with validation and global exception handling.

--- BillingApp.WebApi/BillingApp.WebApi/Program.cs
+++ BillingApp.WebApi/BillingApp.WebApi/Program.cs
@@ -1,12 +1,18 @@
+using BillingApp.WebApi.ExceptionHandling;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
